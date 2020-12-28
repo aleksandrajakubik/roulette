f069f13a2391f081e68ac237d208b177a2e2b138
aleksandrajakubik <aleksandra.jakubik@outlook.com>
2020-12-28 17:46:50 +0100

    api and front template

--- api/routes/rouletteApi.js
+++ api/routes/rouletteApi.js
@@ -1,13 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
 const express = require("express");
 const router = express.Router();
 
 let games = [];
 
-router.get("/games", function(req, res, next) {
+router.get("/games", function(req, res) {
     if(games.length === 0){
         return res.send(false)
     }
     return res.send(games)
 });
+
+
 
 module.exports = router;

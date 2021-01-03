6ce085e6d21538b73cea3ac60c6ea924e33c930f
aleksandrajakubik <aleksandra.jakubik@outlook.com>
2021-01-03 15:52:21 +0100

    Home fixed

--- roulette/src/components/Home.js
+++ roulette/src/components/Home.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from "react-redux";
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from "react-router-dom";
+import { connectToMQTT } from '../store/actions/gameAction'
 
 function Home() {
+
+  useEffect(() => {
+    connectToMQTT()
+  }, [])
 
     const useStyles = makeStyles((theme) => ({
         margin: {
@@ -18,10 +24,12 @@ function Home() {
             <Link to="/login" style={{ textDecoration: 'none' }}>
             <Button className={classes.margin} variant="contained">New Game</Button>
             </Link>
+            <Link to="/games" style={{ textDecoration: 'none' }}>
             <Button className={classes.margin} variant="contained">Search Games</Button>
+            </Link>
         </div>
     )
 
 }
 
-export default Home;
+export default connect(null, { connectToMQTT })(Home);
